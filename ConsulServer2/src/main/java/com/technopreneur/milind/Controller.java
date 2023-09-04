e878cd6e8462b62bbeafb94e26ff7590e1fe937c
Technopreneur Milind <milind.technopreneur@gmail.com>
2023-09-04 11:07:53 +0530

    Commit ConsulServer2

---
+++ ConsulServer2/src/main/java/com/technopreneur/milind/Controller.java
@@ -0,0 +1,18 @@
+package com.technopreneur.milind;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/server")
+public class Controller {
+
+	@GetMapping("/{key}")
+	public String getValue(@PathVariable String key) {
+		System.out.println("Server2 is hit");
+		return key.toUpperCase();
+	}
+
+}
